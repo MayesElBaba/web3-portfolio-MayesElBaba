b3d84ef4dc19daf2a5fe74375bc3cad1561123c8
Mayes Elbaba <mayes.el.baba@student.ehb.be>
2021-12-19 19:26:26 +0100

    Volledig code -portfolio

---
+++ js/project.js
@@ -0,0 +1,97 @@
+let projects =[
+
+    {
+
+        name: "Movie website",
+        info:" Moviewebsite is a site where you can watch all your favourite movies and shows. You can save all your favourites in a list that will keep it storaged for you to continue watching or to start.Its an easy and effective way to enjoy your screen time wisely.All the movies and shows are pulled out from an extern API",
+        tags: "#webdevelopment",
+        image:"../img/1W.PNG",
+    },
+    {
+
+        name: "ChefsPost Logo",
+        info:"ChefsPost is a design for applications for challenges that can be played daily as well as weekly.",
+        tags: "#design",
+        image:"../img/4D.PNG",
+    },
+
+
+    {
+
+        name: "Yourjobs website",
+        info: "Yourjobs is a website that makes it possible for you to look up new jobs, save jobs, delete jobs and to create lists with jobs that are available. The website is also very handy if you want to keep yourself up to date about jobs. The website will keep everything clear and useful." ,
+        tags: "#webdevelopment",
+        image:"../img/2W.PNG",
+    },
+
+    {
+
+        name: "Tagsafe website",
+        info:" Tagsafe is an journalistic application where you can find and save articles on.",
+        tags: "#webdevelopment",
+        image:"../img/3W.PNG",
+    },
+
+    {
+
+        name: "login",
+        info:"ChefsPost is a design for applications for challenges that can be played daily as well as weekly. The challenges are based on cooking skills. It will provide your skills but will keep your social contacts aswell since it is a challenge between you and your friend that you invite to challenge.",
+        tags: "#appdevelopment",
+        image:"../img/1A.PNG",
+    },
+
+    {
+
+        name: "Guess what it is?",
+        info:"Play and plant is a game. By playing the game you will dubble up your fruits and vegetables seeds.The game helps to motivate the players to stay healthy and to eat their fruits and vegetables.",
+        tags: "#appdevelopment",
+        image:"../img/2A.PNG",
+    },
+
+    {
+
+        name: "ChefsPost App",
+        info:"ChefsPost is a design for applications for challenges that can be played daily as well as weekly. The challenges are based on cooking skills. It will provide your skills but will keep your social contacts aswell since it is a challenge between you and your friend that you invite to challenge.",
+        tags: "#appdevelopment",
+        image:"../img/3A.PNG",
+    },
+    {
+
+        name: "live chat",
+        info:"Login and live chat application build with React Native",
+        tags: "#appdevelopment",
+        image:"../img/4A.PNG",
+    },
+
+    {
+
+        name: "IOT Poster",
+        info:"A poster about the influence of IOT in our lives within now and 10 years. The way of thinking about what will happen and how things will turn out to be looking like.",
+        tags: "#design",
+        image:"../img/3D.PNG",
+    },
+    {
+
+        name: "To Do list",
+        info:"To Do list application build with React Native. You can Add and remove your todo's",
+        tags: "#appdevelopment",
+        image:"../img/5A.PNG",
+    },
+
+    {
+
+        name: "ChefsPost Poster",
+        info:"ChefsPost is a design for applications for challenges that can be played daily as well as weekly.",
+        tags: "#design",
+        image:"../img/1D.PNG",
+    },
+    {
+
+        name: "Palestinian restaurant menu",
+        info:"Menu Design for the Palestinian food",
+        tags: "#design",
+        image:"../img/2D.PNG",
+    }
+
+
+]
